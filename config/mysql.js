809f62637ea9556a08fd70446b36ddb198def8d5
Matt Bomhoff <mbomhoff@email.arizona.edu>
2018-08-09 12:24:40 -0700

    Add missing mysql config

---
+++ config/mysql.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Load config file
+var config = require('../config.json');
+
+// Initialize MySQL connection via ORM
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(config.mysql.database, config.mysql.user, config.mysql.password, {
+    host: config.mysql.host,
+    dialect: 'mysql',
+    define: {
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true
+    },
+    operatorsAliases: false
+});
+module.exports.sequelize = sequelize;
