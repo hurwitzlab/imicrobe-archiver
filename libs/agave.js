63bd27755e7288817f8ee0ae7f2533cdb0b48151
Matt Bomhoff <mbomhoff@email.arizona.edu>
2018-08-20 16:43:44 -0700

    Add handling for bad status and timeout

--- libs/agave.js
+++ libs/agave.js
@@ -12,7 +12,7 @@ class AgaveAPI {
 
         // Request file from Agave
         return new Promise(function(resolve, reject) {
-            console.log("AgaveAPI.filesGet " + remotePath + " " + localPath);
+            console.log("AgaveAPI.filesGet", self.token, remotePath, localPath);
 
             var options = {
                 host: config.agaveConfig.baseUrl.replace(/^https?:\/\//,''), // remove protocol
@@ -25,6 +25,11 @@ class AgaveAPI {
             var file = fs.createWriteStream(localPath);
 
             var request = https.get(options, response => {
+                if (!('' + response.statusCode).match(/^2\d\d$/)) {
+                    console.log("http status", response.statusCode);
+                    reject();
+                }
+
                 response.pipe(file);
 
                 response.on("end", function() {
@@ -36,6 +41,11 @@ class AgaveAPI {
             request.on("error", function(error) {
                 console.log("https error:", error);
                 reject(error);
+            });
+
+            request.on('timeout', function () {
+                console.log("https timeout");
+                reject();
             });
         });
     }
