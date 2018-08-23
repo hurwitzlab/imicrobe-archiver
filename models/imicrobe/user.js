bdcb708f0291f236fa8f9a1da741e28fec55c869
Matt Bomhoff <mbomhoff@email.arizona.edu>
2018-08-23 11:54:58 -0700

    Add user model

---
+++ models/imicrobe/user.js
@@ -0,0 +1,40 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('user', {
+    user_id: {
+      type: DataTypes.INTEGER(10).UNSIGNED,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    user_name: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true
+    },
+    first_name: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+    },
+    last_name: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    date: {
+      type: DataTypes.TIME,
+      allowNull: false,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    orcid: {
+      type: DataTypes.STRING(30),
+      allowNull: true
+    }
+  }, {
+    tableName: 'user'
+  });
+};
